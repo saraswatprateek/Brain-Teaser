0c3c758fa13a94a39b8f745336e85f391c7790e9
Prateek Saraswat <saraswat.prateek100@gmail.com>
2017-12-17 21:49:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/prateek/braintrainer/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.prateek.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button startButton;
+    public void start (View view) {
+
+        startButton.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        TextView sumTextView = (TextView)findViewById(R.id.sumTextView);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b) );
+
+    }
+}
